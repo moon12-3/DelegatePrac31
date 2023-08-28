991b8a08380980f4dd01a2484e6da84680db45e3
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-28 11:01:24 +0900

    ForEach를 활용한 순회 처리

--- DelegatePrac31/Program.cs
+++ DelegatePrac31/Program.cs
@@ -12,6 +12,11 @@ namespace DelegatePrac31
         {
             public string Name { get; set; }
             public int Price { get; set; }
+
+            public override string ToString()
+            {
+                return this.Name + " : " + this.Price;
+            }
         }
         static void Main(string[] args)
         {
@@ -25,19 +30,29 @@ namespace DelegatePrac31
             };
             // 정렬
             // 1. Delegator를 활용한 구현 방법
-            products.Sort(SortWithPrice);
+           /* products.Sort(SortWithPrice);*/
 
             // 2. 무명 Delegator를 활용한 구현 방법
-            products.Sort(delegate (Product x, Product y)
+            /*products.Sort(delegate (Product x, Product y)
             {
                 return x.Price.CompareTo(y.Price);
-            });
+            });*/
+
+            // 3. 람다를 활용한 구현 방법1
+            /*products.Sort((x, y) => {
+                return x.Price.CompareTo(y.Price);
+            });*/
+
+            // 3. 람다를 활용한 구현 방법2
+            products.Sort((x, y)=> x.Price.CompareTo(y.Price));
+
+            products.ForEach(p => Console.WriteLine(p));
 
             //출력
-            foreach (var item in products)
+            /*foreach (var item in products)
             {
                 Console.WriteLine(item.Name + " : " + item.Price);
-            }
+            }*/
         }
 
         private static int SortWithPrice(Product x, Product y)
